e7cb3695c6f76be3e6be86bccc8168665e531bed
hientuminh <hientuminh@gmail.com>
2019-12-12 17:06:08 +0700

    [part2d] Finish example

--- Examples/part2-altering-data-server/src/App.js
+++ Examples/part2-altering-data-server/src/App.js
@@ -51,6 +51,10 @@ const App = () => {
       .then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
+      .catch(error => {
+        alert(`the note '${note.content}' was already deleted from server`)
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
 
   const rows = () => notesToShow.map(note =>
