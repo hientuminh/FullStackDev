1808ef98949027f2e82617e32eb20f69a7c8c470
hientuminh <hientuminh@gmail.com>
2019-12-20 14:19:19 +0700

    [part4b] Add example

---
+++ Examples/part4b-testing-backend/app.js
@@ -0,0 +1,32 @@
+const config = require('./utils/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(bodyParser.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/notes', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- Study/part04/lesson.md
+++ Study/part04/lesson.md
@@ -20,3 +20,23 @@
 │   ├── config.js
 │   └── middleware.js  
 ```
+
+# Testing in backend
+- lib
+  - mongo-mock
+  - cross-env: fix on window
+  - supertest: write tests for testing the API
+```
+test('there are four notes', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body.length).toBe(4)
+})
+```
+- Async/sync
+```
+notesRouter.get('/', async (request, response) => {
+ const notes = await Note.find({})
+ response.json(notes.map(note => note.toJSON()))
+})
+```
